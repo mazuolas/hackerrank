f9a5ece475a6e8baa58d858395110608245054f4
Mark Azuolas <markazuolas@gmail.com>
2017-11-08 12:03:27 -0800

    copy boilerplate

---
+++ kangaroo.rb
@@ -0,0 +1,11 @@
+def kangaroo(x1, v1, x2, v2)
+    # Complete this function
+end
+
+x1, v1, x2, v2 = gets.strip.split(' ')
+x1 = x1.to_i
+v1 = v1.to_i
+x2 = x2.to_i
+v2 = v2.to_i
+result = kangaroo(x1, v1, x2, v2)
+puts result;
