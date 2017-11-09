4df7127aac909dfdaa6072764536cbd1ee8e1cf6
Mark Azuolas <markazuolas@gmail.com>
2017-11-09 12:26:21 -0800

    first attempt twosets

--- between_two_sets.rb
+++ between_two_sets.rb
@@ -2,6 +2,19 @@
 
 def getTotalX(a, b)
     # Complete this function
+    count = 0
+    (a[-1]..b[0]).each do |i|
+      valid = true
+      a.each do |el|
+        valid = false if i % el != 0
+      end
+      next unless valid
+      b.each do |el|
+        valid = false if el % i != 0
+      end
+      count += 1 if valid
+    end
+    count
 end
 
 n, m = gets.strip.split(' ')
