c38705e483757fe83c0ed518a5da6370ff3a8ee8
Mark Azuolas <markazuolas@gmail.com>
2017-11-09 12:32:05 -0800

    breaking the records solution

--- breaking_the_record.rb
+++ breaking_the_record.rb
@@ -2,6 +2,21 @@
 
 def getRecord(s)
     # Complete this function
+    high_record = s[0]
+    low_record = s[0]
+    high_count = 0
+    low_count = 0
+    s.each do |score|
+      if score > high_record
+        high_record = score
+        high_count += 1
+      end
+      if score < low_record
+        low_record = score
+        low_count += 1
+      end
+    end
+    [high_count, low_count]
 end
 
 n = gets.strip.to_i
