0554bc8ba19a3c0b52650daf5fd4ea8f21528d25
Mark Azuolas <markazuolas@gmail.com>
2017-11-09 12:33:00 -0800

    start birthday chocolate

---
+++ birthday_chocolate.rb
@@ -0,0 +1,14 @@
+#!/bin/ruby
+
+def solve(n, s, d, m)
+    # Complete this function
+end
+
+n = gets.strip.to_i
+s = gets.strip
+s = s.split(' ').map(&:to_i)
+d, m = gets.strip.split(' ')
+d = d.to_i
+m = m.to_i
+result = solve(n, s, d, m)
+puts result;
