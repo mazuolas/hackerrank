1b3ffb3877cb0c992a7306f1087fbb260f2b2390
Mark Azuolas <markazuolas@gmail.com>
2018-01-08 16:16:14 -0800

    start divisible-sum-pairs

---
+++ divisible-sum-pairs.rb
@@ -0,0 +1,13 @@
+#!/bin/ruby
+
+def divisibleSumPairs(n, k, ar)
+    # Complete this function
+end
+
+n, k = gets.strip.split(' ')
+n = n.to_i
+k = k.to_i
+ar = gets.strip
+ar = ar.split(' ').map(&:to_i)
+result = divisibleSumPairs(n, k, ar)
+puts result;
