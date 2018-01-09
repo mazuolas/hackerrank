908dbaa32f4d2a33766e5f9e82e92d7a9ce8a54f
Mark Azuolas <markazuolas@gmail.com>
2018-01-08 16:43:43 -0800

    first attempt

--- divisible-sum-pairs.rb
+++ divisible-sum-pairs.rb
@@ -2,6 +2,13 @@
 
 def divisibleSumPairs(n, k, ar)
     # Complete this function
+    pairs = []
+    ar.each_with_index do |el, i|
+      ((i+1)...n).each do |j|
+        pairs << [i,j] if ( el < ar[j] && ((el+ar[j]) % k == 0))
+      end
+    end
+    pairs.length
 end
 
 n, k = gets.strip.split(' ')
