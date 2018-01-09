c97a1c0dc9ea8b91a18ea31d40443b17f620ceaa
Mark Azuolas <markazuolas@gmail.com>
2018-01-08 18:20:37 -0800

    change seen array to hash to improve time

--- bfsshortreach.rb
+++ bfsshortreach.rb
@@ -14,16 +14,16 @@ class Node
   def find_distance(target)
     distance = 6
     queue = @edges.values.dup
-    seen = queue.dup
+    seen = @edges.dup
     until queue.empty?
       return distance if queue.include?(target)
       distance += 6
       children = []
       queue.each do |n|
         n.edges.values.each do |e|
-          unless seen.include?(e)
+          unless seen[e]
             children << e
-            seen << e
+            seen[e] = true
           end
         end
       end
