a80a698b2c69553ed168f9e7955c1ce52e22fe87
Mark Azuolas <markazuolas@gmail.com>
2018-01-08 17:49:25 -0800

    change test

--- bfsshortreach.rb
+++ bfsshortreach.rb
@@ -57,25 +57,25 @@ def bfs(n, m, edges, s)
     distances
 end
 
-# q = gets.strip.to_i
-# for a0 in (0..q-1)
-#     n, m = gets.strip.split(' ')
-#     n = n.to_i
-#     m = m.to_i
-#     edges = Array.new(m)
-#     for edges_i in (0..m-1)
-#         edges_t = gets.strip
-#         edges[edges_i] = edges_t.split(' ').map(&:to_i)
-#     end
-#     s = gets.strip.to_i
-#     result = bfs(n, m, edges, s)
-#     print result.join(" ")
-#
-#
-# end
+q = gets.strip.to_i
+for a0 in (0..q-1)
+    n, m = gets.strip.split(' ')
+    n = n.to_i
+    m = m.to_i
+    edges = Array.new(m)
+    for edges_i in (0..m-1)
+        edges_t = gets.strip
+        edges[edges_i] = edges_t.split(' ').map(&:to_i)
+    end
+    s = gets.strip.to_i
+    result = bfs(n, m, edges, s)
+    print result.join(" ")
 
-n = 4
-m = 2
-edges = [[1,2],[1,3]]
-s = 1
-p bfs(n, m, edges, s)
+
+end
+
+# n = 4
+# m = 2
+# edges = [[1,2],[1,3]]
+# s = 1
+# p bfs(n, m, edges, s)
