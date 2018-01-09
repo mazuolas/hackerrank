9cce1c0b573d92f73ed6232f72a8cf7904e2fa08
Mark Azuolas <markazuolas@gmail.com>
2018-01-08 18:44:36 -0800

    first submit - too slow

--- recursive-digit-sum.rb
+++ recursive-digit-sum.rb
@@ -1,7 +1,14 @@
 #!/bin/ruby
 
-def digitSum(n, k)
+def digitSum(n, k = 1)
     # Complete this function
+    return n if n < 10
+    sum = 0
+    until n = 0
+      sum += n % 10
+      n = n/10
+    end
+    digitSum(sum*k)
 end
 
 n, k = gets.strip.split(' ')
