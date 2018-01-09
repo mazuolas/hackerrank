2e611b42568d6321c0705823c125fd07d34e0344
Mark Azuolas <markazuolas@gmail.com>
2018-01-08 18:26:52 -0800

    start fib-modified

---
+++ fib-modified.rb
@@ -0,0 +1,12 @@
+#!/bin/ruby
+
+def fibonacciModified(t1, t2, n)
+    # Complete this function
+end
+
+t1, t2, n = gets.strip.split(' ')
+t1 = t1.to_i
+t2 = t2.to_i
+n = n.to_i
+result = fibonacciModified(t1, t2, n)
+puts result
